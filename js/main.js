5bb3c7574de701b00e1bf63c8a9e7f16681d805e
Jumpei Asanuma <jumpei@JumpeinoMacBook-ea.local>
2020-07-09 20:55:15 +0900

    修正しました

--- js/main.js
+++ js/main.js
@@ -1,56 +1,118 @@
-　'use strict'
-  
-  const btn = document.getElementById('btn');
-  const taskList = [];
-  let i = 0;
+'use strict';
 
-  btn.addEventListener('click',()=> {
-    const tr = document.createElement('tr');
+const btn = document.getElementById('btn');
+const todos = [];
+const todo = {
+  task:'',
+  status: '作業中',
+};
+let i = 0;
 
-   //タスク入力値
-   function inp_task () {
-    const text = document.getElementById('input_task');
-    let input = text.value;
-    taskList.push(input);
-    const tdEl_task = document.createElement('td');
-    const tdEl_num = document.createElement('td');
-    for (let i = 0; i < taskList.length; i++){
-      tdEl_num.innerHTML = `${i}`;
-      tdEl_task.innerHTML = `${taskList[i]}`;
-    }
-    tr.appendChild(tdEl_num);
-    tr.appendChild(tdEl_task);
-    text.value = '';
-    text.focus(); 
-    }
+btn.addEventListener('click', () => {
+  const tr = document.createElement('tr');
+  let text = document.getElementById('input_task');
+  let textValue = text.value;
+  todo.task = textValue;
+  todos.push(todo);
   
 
-  　　 //作業中ボタン
-  function con_btn() {
-    const tdEl_con = document.createElement('td');
-    const inpEl_con = document.createElement('input');
-    inpEl_con.setAttribute("class","inp_con");
-    inpEl_con.setAttribute("type","button");
-    inpEl_con.setAttribute("value","作業中");
-    tdEl_con.appendChild(inpEl_con);
-    tr.appendChild(tdEl_con);
-  }
+  //Todoを表示させる
+  const displayTodos = function() {
+    const tdTask = document.createElement('td');
+    const tdNum = document.createElement('td');
+    for (let i = 0; i < todos.length; i++){
+            tdNum.innerHTML = `${i}`;
+            tdTask.innerHTML = `${todos[i].task}`;
+          };
 
-    //削除ボタン
-    function del_btn() {
-      const tdEl_del = document.createElement('td');
-      const inpEl_del = document.createElement('input');
-      inpEl_del.setAttribute("class","inp_del");
-      inpEl_del.setAttribute("type","button");
-      inpEl_del.setAttribute("value","削除");
-      tdEl_del.appendChild(inpEl_del);
-      tr.appendChild(tdEl_del);
+    // 状態ボタン（通常は作業中）
+    function statusBtn() {
+        const tdStatus = document.createElement('td');
+        const statusBtn = document.createElement('button');
+        statusBtn.textContent = todos[i].status;
+        tdStatus.appendChild(statusBtn);
+        tr.appendChild(tdStatus);
     }
 
-      inp_task();
-      con_btn();
-      del_btn();
-      
-      document.querySelector('table').appendChild(tr);
+    //  削除ボタン
+    function delBtn() {
+        const tdDelete= document.createElement('td');
+        const delBtn = document.createElement('button');
+        delBtn.textContent = "削除";
+        tdDelete.appendChild(delBtn);
+        tr.appendChild(tdDelete);
+    }
+        tr.appendChild(tdNum);
+        tr.appendChild(tdTask);
+        
+        text.value = '';
+        text.focus(); 
+        return statusBtn(), delBtn();
+
+    }
+    displayTodos();
+    document.querySelector('table').appendChild(tr);
 
   });
+
+
+
+// 　'use strict'
+  
+//   let todo = {
+//     content: '入力されたタスク',
+//     status: '作業中',
+//   }
+//   const todos = [];
+//   const btn = document.getElementById('btn');
+//   let i = 0;
+
+//   btn.addEventListener('click',()=> {
+//     const tr = document.createElement('tr');
+
+//   //   　 //作業中ボタン
+//    function statusBtn() {
+//     const tdEl_con = document.createElement('td');
+//     const inpEl_con = document.createElement('button');
+//     inpEl_con.textContent = "作業中";
+//     tdEl_con.appendChild(inpEl_con);
+//     tr.appendChild(tdEl_con);
+//   }
+
+//   // 削除ボタン
+//     function delBtn() {
+//       const tdEl_del = document.createElement('td');
+//       const inpEl_del = document.createElement('button');
+//       inpEl_del.textContent = "削除";
+//       tdEl_del.appendChild(inpEl_del);
+//       tr.appendChild(tdEl_del);
+//     }
+
+//    //タスク入力値
+//    function showTask () {
+//     const text = document.getElementById('input_task');
+//     let input = text.value;
+//     todo.content = input;
+//     todos.push(input);
+//     const tdEl_task = document.createElement('td');
+//     const tdEl_num = document.createElement('td');
+//     console.log(todo);
+//     for (let i = 0; i < todos.length; i++){
+//       console.log(todos);
+//       tdEl_num.innerHTML = `${i}`;
+//       tdEl_task.innerHTML = `${todos[i]}`;
+//     }
+//     tr.appendChild(tdEl_num);
+//     tr.appendChild(tdEl_task);
+    
+//     text.value = '';
+//     text.focus(); 
+
+//     return statusBtn(), delBtn();
+//     }
+  
+// showTask();
+      
+//       document.querySelector('table').appendChild(tr);
+
+//   });
